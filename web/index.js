2c39a73d5713eeb03cad8282bf85804363de3141
Lowder <me@lowderplay.dev>
2021-09-05 10:32:09 +0500

    moved to mysql

--- web/index.js
+++ web/index.js
@@ -1,67 +1,93 @@
+const port = 3000;
+
+const mysql = require('mysql');
 const express = require('express');
 const csv = require('fast-csv');
 const fs = require('fs');
 const axios = require('axios');
 const cookieParser = require("cookie-parser");
 
+const connection = mysql.createConnection({
+    host     : 'mysql',
+    user     : 'root',
+    password : 'root',
+    database : 'dataset'
+});
+connection.connect(function(err) {
+    if (err) {
+        return console.error('error connecting: ' + err.stack);
+    }
+    console.log('connected as id ' + connection.threadId);
+    app.listen(port,()=>{
+        console.log(`listening on ${port}`)
+    })
+});
+
 const app = express();
 app.use(cookieParser());
-
-const port = 3000;
-const items = [
-    // {name: "Ручка шариковая", cost: "10руб.", offers: 1},
-    // {name: "Блокнот", cost: "15руб.", offers: 2},
-    // {name: "Маркер", cost: "20руб.", offers: 1},
-    // {name: "Шапка", cost: "55руб.", offers: 1},
-    // {name: "Батарейки AAA", cost: "15руб.", offers: 5},
-];
-fs.createReadStream('dataset.csv')
-    .pipe(csv.parse({ headers: true, delimiter: ';' }))
-    .on('error', error => console.error(error))
-    .on('data', row => {
-        items.push({...row,
-            info: JSON.parse(row.info),
-            offers: JSON.parse(row.offers).map(o=>o["Name"]),
-            price: row.price === "NULL" ? "Нет в наличии" : JSON.parse(row.price)[0]["Cost"]+" руб."
-        })
-    })
-    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`, items));
 
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 
-app.get('/cart', async (req, res) => {
-    const cart = JSON.parse(req.cookies.cart).map(i=>i.toString());
-    const limited = await Promise.all(addImages(items.filter(i=> cart.includes(i.id))));
-    res.render("cart", {
-        items: limited,
-        recommended: await Promise.all(addImages(items.slice(0, 3)))
-    });
+app.get('/cart', (req, res) => {
+    const cart = JSON.parse(req.cookies.cart).join(",");
+    connection.query(`SELECT * FROM dataset_new WHERE \`Идентификатор СТЕ\` IN (${cart});`,
+        async function (error, results, fields) {
+            if (error && cart.length !== 0) throw error;
+            res.render("cart", {
+                items: await Promise.all(addImages(cart.length !== 0 ? results : [])),
+                recommended: []
+            });
+        });
+
+
 })
 app.get('/item/:id', async (req, res) => {
     const id = req.params.id;
-    if(!items.map(i=>i.id).includes(id)) return res.sendStatus(404);
+    connection.query(`SELECT * FROM dataset_new WHERE \`Идентификатор СТЕ\` = ${id};`,
+        async function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            connection.query(`SELECT * FROM dataset_new WHERE 
+(\`Категория\`= '${results[0]['Категория']}' AND \`Идентификатор СТЕ\` <> ${id}) 
+ORDER BY \`Просмотры\` DESC LIMIT 4`,
+                async function (error, recommendations, fields) {
+                    if (error) throw error;
+                    const recommended = await Promise.all(addImages(recommendations));
+                    res.render("item", {
+                        item: await addImages(results)[0],
+                        recommended
+                    });
+                });
+        });
 
-    const info = await axios.get(`https://old.zakupki.mos.ru/api/Cssp/Sku/GetEntity?id=${id}`);
-    res.render("item", {
-        item: {...items.filter(i => i.id === id)[0], imageId: info.data.images[0].fileStorage.id},
-        recommended: await Promise.all(addImages(items.slice(0, 3)))
-    });
+    // const info = await axios.get(`https://old.zakupki.mos.ru/api/Cssp/Sku/GetEntity?id=${id}`);
+    // res.render("item", {
+    //     item: {...items[id], image: '/static/image.svg'},
+    //     recommended: await Promise.all(addImages(sorted.map(id=>items[id])
+    //         .filter(i=>i.category === items[id].category).slice(0, 3)))
+    // });
 })
 
 app.get('/', async (req, res) => {
-    const limited = await Promise.all(addImages(items.slice(0, 12)))
-    res.render("catalog", {items: limited});
+    connection.query(`SELECT * FROM dataset_new ORDER BY \`Просмотры\` DESC LIMIT 12`,
+        async function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            res.render("catalog", {items: await Promise.all(addImages(results))});
+        });
+
 })
 
-app.listen(port, () => {
-    console.log(`listening at :${port}`);
-})
 
 function addImages(items) {
     return items.map(async item => {
-        const info = await axios.get(`https://old.zakupki.mos.ru/api/Cssp/Sku/GetEntity?id=${item.id}`);
-        return {...item, imageId: info.data.images[0].fileStorage.id};
+        const info = await axios
+            .get(`https://old.zakupki.mos.ru/api/Cssp/Sku/GetEntity?id=${item['Идентификатор СТЕ']}`);
+        return {
+            ...item,
+            image: `https://zakupki.mos.ru/newapi/api/Core/Thumbnail/${info.data.images[0].fileStorage.id}/300/300`
+        };
     });
 }
